77a673fd6389cb43007118ecc21ef348195104fa
Suraj Patil <sapatil@live.com>
2016-12-12 11:57:35 +0530

    updated URLS

--- main.go
+++ main.go
@@ -40,31 +40,31 @@ func main() {
 
 	http.Handle("/static/", http.FileServer(http.Dir("public")))
 
-	r.HandleFunc("/api/task/", views.GetTasksFuncAPI).Methods("GET")
-	r.HandleFunc("/api/task/", views.AddTaskFuncAPI).Methods("PUT")
-	r.HandleFunc("/api/task/", views.UpdateTaskFuncAPI).Methods("POST")
-	r.HandleFunc("/api/task/{id}", views.TrashTaskFuncAPI).Methods("DELETE")
-	r.HandleFunc("/api/deleted/", views.GetDeletedTaskFuncAPI).Methods("GET")
+	r.HandleFunc("/task/", views.GetTasksFuncAPI).Methods("GET")
+	r.HandleFunc("/task/", views.AddTaskFuncAPI).Methods("PUT")
+	r.HandleFunc("/task/", views.UpdateTaskFuncAPI).Methods("POST")
+	r.HandleFunc("/task/{id}", views.TrashTaskFuncAPI).Methods("DELETE")
+	r.HandleFunc("/deleted/", views.GetDeletedTaskFuncAPI).Methods("GET")
 
-	r.HandleFunc("/api/completed/", views.GetCompletedTaskFuncAPI).Methods("GET")
-	r.HandleFunc("/api/categories/", views.GetCategoryFuncAPI).Methods("GET")
+	r.HandleFunc("/completed/", views.GetCompletedTaskFuncAPI).Methods("GET")
+	r.HandleFunc("/categories/", views.GetCategoryFuncAPI).Methods("GET")
 
-	r.HandleFunc("/api/category/{category}", views.ShowCategoryFuncAPI).Methods("GET")
-	r.HandleFunc("/api/category/{category}", views.DeleteCategoryFuncAPI).Methods("DELETE")
-	r.HandleFunc("/api/complete-task/{id}", views.CompleteTaskFuncAPI).Methods("GET")
-	r.HandleFunc("/api/incomplete-task/{id}", views.RestoreFromCompleteFuncAPI).Methods("GET")
-	r.HandleFunc("/api/restore-task/{id}", views.RestoreTaskFuncAPI).Methods("GET")
+	r.HandleFunc("/category/{category}", views.ShowCategoryFuncAPI).Methods("GET")
+	r.HandleFunc("/category/{category}", views.DeleteCategoryFuncAPI).Methods("DELETE")
+	r.HandleFunc("/complete-task/{id}", views.CompleteTaskFuncAPI).Methods("GET")
+	r.HandleFunc("/incomplete-task/{id}", views.RestoreFromCompleteFuncAPI).Methods("GET")
+	r.HandleFunc("/restore-task/{id}", views.RestoreTaskFuncAPI).Methods("GET")
 	r.HandleFunc("/", views.Home).Methods("GET")
 
-	r.HandleFunc("/api/comment/", views.AddCommentFuncAPI).Methods("PUT")
-	r.HandleFunc("/api/comment/{id}", views.DeleteCommentFuncAPI).Methods("DELETE")
-	r.HandleFunc("/api/category/", views.AddCategoryFuncAPI).Methods("PUT")
-	r.HandleFunc("/api/category/{category}", views.UpdateCategoryFuncAPI).Methods("POST")
+	r.HandleFunc("/comment/", views.AddCommentFuncAPI).Methods("PUT")
+	r.HandleFunc("/comment/{id}", views.DeleteCommentFuncAPI).Methods("DELETE")
+	r.HandleFunc("/category/", views.AddCategoryFuncAPI).Methods("PUT")
+	r.HandleFunc("/category/{category}", views.UpdateCategoryFuncAPI).Methods("POST")
 
 	//Login logout
-	r.HandleFunc("/api/login/", views.LoginFuncAPI).Methods("POST", "GET")
-	r.HandleFunc("/api/logout/", views.RequiresLogin(views.LogoutFuncAPI)).Methods("GET")
-	r.HandleFunc("/api/signup/", views.SignUpFuncAPI).Methods("PUT")
+	r.HandleFunc("/login/", views.LoginFuncAPI).Methods("POST", "GET")
+	r.HandleFunc("/logout/", views.RequiresLogin(views.LogoutFuncAPI)).Methods("GET")
+	r.HandleFunc("/signup/", views.SignUpFuncAPI).Methods("PUT")
 
 	//these handlers perform action like delete, mark as complete etc
 	// http.HandleFunc("/files/", views.RequiresLogin(views.UploadedFileHandler))
