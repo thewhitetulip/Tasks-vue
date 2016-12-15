ced5e5f3e94adc9c70b39b16ac8c5cfcf24dd041
Suraj Patil <sapatil@live.com>
2016-12-15 14:54:47 +0530

    consolidated views

--- views/views.go
+++ views/views.go
@@ -1,9 +1,8 @@
 package views
 
-/*Holds the fetch related view handlers*/
-
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -13,6 +12,21 @@ var templates *template.Template
 var message string //message will store the message to be shown as notification
 var err error
 
+// Home renders the basic html page to the Vue front end, the Vue front end will then
+// update the html content according to the user interactions by making AJAX calls
+// we have used Go language's templating mechanism to split templates into logical parts
 func Home(w http.ResponseWriter, r *http.Request) {
 	homeTemplate.Execute(w, nil)
+}
+
+// PopulateTemplates parses all templates present in
+// the templates folder and returns a pointer in templates
+// we use templates variable to locate other templates
+func PopulateTemplates() {
+	templates, err = template.ParseGlob("./templates/*.html")
+	if err != nil {
+		log.Println(err)
+	}
+	homeTemplate = templates.Lookup("tasks.html")
+
 }
